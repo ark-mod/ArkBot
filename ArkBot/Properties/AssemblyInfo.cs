1aa3f9e353c9ad0e952209677f1483cc39ed71ca
Tobias Sebring <tsebring@gmail.com>
2017-05-08 08:41:19 +0200

    Aliases updated for v257

--- ArkBot/Properties/AssemblyInfo.cs
+++ ArkBot/Properties/AssemblyInfo.cs
@@ -44,5 +44,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.40.*")]
-[assembly: AssemblyFileVersion("1.40.0.0")]
+[assembly: AssemblyVersion("1.41.*")]
+[assembly: AssemblyFileVersion("1.41.0.0")]
