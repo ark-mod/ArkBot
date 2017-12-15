b7da30dcb6d7f9b5b030c61c17ec84692e2998a1
Tobias Sebring <tsebring@gmail.com>
2017-12-15 15:49:49 +0100

    Aberration map

--- ArkBot/Properties/AssemblyInfo.cs
+++ ArkBot/Properties/AssemblyInfo.cs
@@ -44,5 +44,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.79.*")]
-[assembly: AssemblyFileVersion("1.79.0.0")]
+[assembly: AssemblyVersion("1.80.*")]
+[assembly: AssemblyFileVersion("1.80.0.0")]
