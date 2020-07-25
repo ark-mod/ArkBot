101359c39a79aa7c586d636de9e6e4958b42a35f
mxtsdev <mxtsdev@gmail.com>
2020-07-25 09:24:07 +0200

    fixed obelisk item reading

--- ArkBot/Data/ArkItems.cs
+++ ArkBot/Data/ArkItems.cs
@@ -180,7 +180,7 @@ namespace ArkBot.Data
             [JsonProperty("description")]
             public string Description { get; set; }
 
-            [JsonProperty("blueprintPath")]
+            [JsonProperty("bp")]
             public string BlueprintPath { get; set; }
 
             [JsonProperty("type")]
